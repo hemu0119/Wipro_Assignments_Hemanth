d5f999672cce719d37a151d228d4f2e74ec9658e
hemu0119 <2100080119ai.ds@gmail.com>
2026-06-12 00:25:07 +0530

    Add files via upload

---
+++ Day27/CrossBrowser.java
@@ -0,0 +1,46 @@
+package Day27;
+
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CrossBrowser {
+
+    WebDriver driver;
+
+    @Parameters("browser")
+    @Test
+    public void launchBrowser(String browser) throws Exception {
+
+        if(browser.equalsIgnoreCase("chrome"))
+        {
+            ChromeOptions options = new ChromeOptions();
+
+            driver = new RemoteWebDriver(
+                    new URL("http://localhost:4444"),
+                    options);
+        }
+        else if(browser.equalsIgnoreCase("edge"))
+        {
+            EdgeOptions options = new EdgeOptions();
+
+            driver = new RemoteWebDriver(
+                    new URL("http://localhost:4444"),
+                    options);
+        }
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.saucedemo.com");
+
+        System.out.println("Browser Launched : " + browser);
+        System.out.println("Title : " + driver.getTitle());
+
+        driver.quit();
+    }
+}
