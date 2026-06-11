8d098ad3043b9a6f3c2f1d11706aa3134bac4970
hemu0119 <2100080119ai.ds@gmail.com>
2026-06-11 23:13:10 +0530

    Add files via upload

---
+++ Day21/launchdemo.java
@@ -0,0 +1,28 @@
+package Day21;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class launchdemo {
+public static void main(String[] args) throws InterruptedException {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.saucedemo.com/");
+	driver.manage().window().maximize();
+	Thread.sleep(3000);
+	
+	driver.findElement(By.id("user-name")).sendKeys("standard_user");
+	Thread.sleep(1000);
+	
+	driver.findElement(By.id("password")).sendKeys("secret_sauce");
+	Thread.sleep(1000);
+	
+	driver.findElement(By.id("login-button")).click();
+	Thread.sleep(1000);
+	
+	System.out.println(driver.getTitle());
+	
+	driver.quit();
+}
+}
